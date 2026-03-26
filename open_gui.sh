ed81e52254dc20190413a759363b0ee928697be3
rafapolo <rafael@extrapolo.com>
2026-03-26 12:01:46 +0100

    docs: reorder README for data users, remove unused files (xdg-open, gera_schemas.py, open_gui.sh, docs/)

--- open_gui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-INIT=$(mktemp /tmp/duckdb_init_XXXX)
-printf "LOAD httpfs;\nATTACH 'basedosdados.duckdb' AS bd (READ_ONLY);\n" > "$INIT"
-duckdb --ui ui.duckdb -init "$INIT"
-rm -f "$INIT"
